35b6cdcf3b892e3905c30d2bcf5ef06e6e3fea1a
ThiagoSerafim01 <thiago.serafim@sptech.school>
2022-11-29 22:55:49 -0300

    Ultimos testes feitos, funcionando tudo.

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "rainbowsix",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "1227",
 };
 
 function executar(instrucao) {
